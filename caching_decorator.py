8710aa249ac4c113cb92f23dd7aa47d342ff8eec
Форов Александр <5h9t5@list.ru>
2024-10-02 13:56:40 +0300

    import wraps and add validate if callable(max_size)

--- caching_decorator.py
+++ caching_decorator.py
@@ -8,12 +8,15 @@
 Ограничьте размер кеша до 100 записей. При превышении этого лимита удаляйте
 наиболее старые записи (используйте подход FIFO).
 """
+from functools import wraps
 
 
-def cache_decorator(max_size=5):
+def cache_decorator(max_size=None):
+
     def decorator(func):
         cache = {}
 
+        @wraps(func)
         def wrapper(*args, **kwargs):
             key = args, tuple(kwargs.items())
 
@@ -26,14 +29,18 @@ def cache_decorator(max_size=5):
                 if len(cache) > max_size:
                     first_key = next(iter(cache))
                     del cache[first_key]
-                print(cache)
                 return result
 
         return wrapper
 
+    if callable(max_size):
+        func = max_size
+        max_size = 100
+        return decorator(func)
+
     return decorator
 
 
-@cache_decorator(max_size=2)
+@cache_decorator
 def sum_func(a, b):
     return a + b
