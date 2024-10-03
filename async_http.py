85c3264bc00a287c2032ac3499fea609e0641cdc
Форов Александр <5h9t5@list.ru>
2024-10-03 14:54:25 +0300

    add gather and func load_to_json

--- async_http.py
+++ async_http.py
@@ -27,23 +27,28 @@ urls = [
 async def fetch_urls(urls: list[str], file_path: str):
     connector = aiohttp.TCPConnector(limit=5)
     async with aiohttp.ClientSession(connector=connector) as session:
-        for url in urls:
-            try:
-                async with session.get(url, timeout=5) as respone:
-                    status_code = respone.status
-                    result = {'url': url, 'status_code': status_code}
-            except aiohttp.ClientError as e:
-                print(f'Error: {e}')
-                result = {'url': url, 'status_code': 0}
-            except asyncio.TimeoutError:
-                print(f'Время ожидания истекло при запросе {url}')
-                result = {'url': url, 'status_code': 408}
-            except Exception as e:
-                print(f'Непредвиденная ошибка при запросе {url}: {e}')
-                result = {'url': url, 'status_code': -1}
-            with open(file_path, 'a') as file:
-                json.dump(result, file)
-                file.write('\n')
+        tasks = [load_to_json(session, url, file_path) for url in urls]
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+
+async def load_to_json(session, url: str, file_path: str):
+    with open(file_path, 'a') as file:
+        try:
+            async with session.get(url, timeout=5) as response:
+                status_code = response.status
+                result = {'url': url, 'status_code': status_code}
+        except aiohttp.ClientError as e:
+            print(f'Error: {e}')
+            result = {'url': url, 'status_code': 0}
+        except asyncio.TimeoutError:
+            print(f'Время ожидания истекло при запросе {url}')
+            result = {'url': url, 'status_code': 408}
+        except Exception as e:
+            print(f'Непредвиденная ошибка при запросе {url}: {e}')
+            result = {'url': url, 'status_code': -1}
+
+        json.dump(result, file)
+        file.write('\n')
 
 if __name__ == '__main__':
     asyncio.run(fetch_urls(urls, './results.jsonl'))
