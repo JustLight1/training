ad189333c3339b0473bcd7df364145d2fb66a3f0
Форов Александр <5h9t5@list.ru>
2024-10-04 14:30:23 +0300

    feat(async_http): add asyncio.Queue

--- async_http.py
+++ async_http.py
@@ -11,46 +11,76 @@
 Сохраните все результаты в файл
 """
 
-
 import asyncio
-import aiohttp
 import json
+
+import aiohttp
 from aiofiles import open as aio_open
 
 
 urls = [
     'https://example.com',
+    'https://example.com',
+    'https://example.com',
     'https://httpbin.org/status/404',
-    'https://nonexistent.url'
+    'https://httpbin.org/status/404',
+    'https://httpbin.org/status/404',
+    'https://nonexistent.url',
+    'https://nonexistent.url',
+    'https://nonexistent.url',
+    'https://nonexistent.url',
 ]
+
 
 semaphore = asyncio.Semaphore(5)
 
 
+async def load_to_json(
+        session: aiohttp.ClientSession,
+        queue: asyncio.Queue,
+        file_path: str
+):
+    while True:
+        url = await queue.get()
+        if url is None:
+            queue.task_done()
+            break
+        async with semaphore:
+            try:
+                async with session.get(url, timeout=5) as response:
+                    status_code = response.status
+                    result = {'url': url, 'status_code': status_code}
+            except aiohttp.ClientError as e:
+                print(f'Error: {e}')
+                result = {'url': url, 'status_code': 0}
+            except asyncio.TimeoutError:
+                print(f'Время ожидания истекло при запросе {url}')
+                result = {'url': url, 'status_code': 408}
+            except Exception as e:
+                print(f'Непредвиденная ошибка при запросе {url}: {e}')
+                result = {'url': url, 'status_code': -1}
+
+            async with aio_open(file_path, 'a') as file:
+                await file.write(json.dumps(result) + '\n')
+
+        queue.task_done()
+
+
 async def fetch_urls(urls: list[str], file_path: str):
-    async with semaphore:
-        async with aiohttp.ClientSession() as session:
-            tasks = [load_to_json(session, url, file_path) for url in urls]
-            await asyncio.gather(*tasks, return_exceptions=True)
+    queue = asyncio.Queue(5)
+    async with aiohttp.ClientSession() as session:
+        workers = [asyncio.create_task(
+            load_to_json(session, queue, file_path)) for _ in range(5)]
+        for url in urls:
+            await queue.put(url)
 
+        await queue.join()
 
-async def load_to_json(session, url: str, file_path: str):
-    async with aio_open(file_path, 'a') as file:
-        try:
-            async with session.get(url, timeout=5) as response:
-                status_code = response.status
-                result = {'url': url, 'status_code': status_code}
-        except aiohttp.ClientError as e:
-            print(f'Error: {e}')
-            result = {'url': url, 'status_code': 0}
-        except asyncio.TimeoutError:
-            print(f'Время ожидания истекло при запросе {url}')
-            result = {'url': url, 'status_code': 408}
-        except Exception as e:
-            print(f'Непредвиденная ошибка при запросе {url}: {e}')
-            result = {'url': url, 'status_code': -1}
+        for _ in workers:
+            await queue.put(None)
 
-        await file.write(json.dumps(result) + '\n')
+        await asyncio.gather(*workers)
+
 
 if __name__ == '__main__':
     asyncio.run(fetch_urls(urls, './results.jsonl'))
