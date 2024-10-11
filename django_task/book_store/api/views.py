14b534a32020839db6cbba3b2a9869af553d7233
Форов Александр <5h9t5@list.ru>
2024-10-11 20:43:00 +0300

    feat(views): upd BookBuyAPIView add transaction

--- django_task/book_store/api/views.py
+++ django_task/book_store/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from django.db import transaction
+from django.db.models import F
 
 from book.models import Book, User
 from .serializers import BookSerializer, UserSerializer
@@ -45,10 +47,18 @@ class BookRetrieveUpdateDestroyAPIView(APIView):
 
 class BookBuyAPIView(APIView):
 
+    @transaction.atomic
     def post(self, request, pk):
         book = get_object_or_404(Book.objects.select_for_update(), pk=pk)
+        if book.count <= 0:
+            return Response(
+                {'detail': 'Книги нет в наличии'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
         serializer = BookSerializer(book, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
+            Book.objects.filter(pk=book.pk).update(count=F('count') - 1)
+            book.refresh_from_db()
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
