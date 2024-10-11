38e848fcd1beb243f8c1c0d1279bec41c7cd8ceb
Форов Александр <5h9t5@list.ru>
2024-10-11 22:22:45 +0300

    perf(views): upd BookBuyAPIView

--- django_task/book_store/api/views.py
+++ django_task/book_store/api/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from django.db import transaction
-from django.db.models import F
 
 from book.models import Book, User
 from .serializers import BookSerializer, UserSerializer
@@ -53,12 +52,12 @@ class BookBuyAPIView(APIView):
                 {'detail': 'Книги нет в наличии'},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        serializer = BookSerializer(book, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        Book.objects.filter(pk=book.pk).update(count=F('count') - 1)
-        book.refresh_from_db()
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        book.count -= 1
+        book.save()
+        return Response(
+            {'Кол-во оставшихся книг': book.count},
+            status=status.HTTP_200_OK
+        )
 
 
 class AuthorListCreateAPIView(APIView):
