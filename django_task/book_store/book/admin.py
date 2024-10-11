729d08725c0c9cde313d2b9d07cb0b4a327a0879
Форов Александр <5h9t5@list.ru>
2024-10-11 18:57:04 +0300

    feat(admin): add BookAdmin

--- django_task/book_store/book/admin.py
+++ django_task/book_store/book/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from book.models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'count')
+    search_fields = ('id', 'title', 'author')
+    autocomplete_fields = ('author',)
