d97ca872e6428502a66efd2a37f59180490ef36b
Форов Александр <5h9t5@list.ru>
2024-10-11 18:57:18 +0300

    feat(models): add model Book

--- django_task/book_store/book/models.py
+++ django_task/book_store/book/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=64, verbose_name='Название')
+    author = models.ForeignKey(
+        User,
+        related_name='books',
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    count = models.PositiveSmallIntegerField(default=0, verbose_name='Остаток')
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+    def __str__(self):
+        return self.title
