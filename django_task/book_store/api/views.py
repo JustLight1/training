8f173f9698af9a1b791cd21a6fd014915d138782
Форов Александр <5h9t5@list.ru>
2024-10-11 18:56:52 +0300

    feat(views): add BookListCreateAPIView, BookRetrieveUpdateDestroyAPIView, BookBuyAPIView, AuthorListCreateAPIView and AuthorRetrieveUpdateAPIView

--- django_task/book_store/api/views.py
+++ django_task/book_store/api/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from book.models import Book, User
+from .serializers import BookSerializer, UserSerializer
+
+
+class BookListCreateAPIView(APIView):
+
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookRetrieveUpdateDestroyAPIView(APIView):
+
+    def get(self, request, pk):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        book = get_object_or_404(Book, pk=pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BookBuyAPIView(APIView):
+
+    def post(self, request, pk):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(book)
+        if book.count > 0:
+            book.count -= 1
+            book.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AuthorListCreateAPIView(APIView):
+
+    def get(self, request):
+        authors = User.objects.all()
+        serializer = UserSerializer(authors, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AuthorRetrieveUpdateAPIView(APIView):
+    def get(self, request, pk):
+        author = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(author)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        author = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
