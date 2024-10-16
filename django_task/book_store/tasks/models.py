b388d29679e380946a517ba1fbc94f88a1e7f9e6
Форов Александр <5h9t5@list.ru>
2024-10-16 13:58:20 +0300

    feat(tasks): add Task

---
+++ django_task/book_store/tasks/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('completed', 'Completed'),
+    ]
+
+    task_name = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=50, choices=STATUS_CHOICES, default='pending')
+    worker_id = models.PositiveSmallIntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task_name
