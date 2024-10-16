b250cc677ea286ac2198f0f855bfa2affd9831ad
Форов Александр <5h9t5@list.ru>
2024-10-16 17:15:41 +0300

    feat(tasks/views): add Q

--- django_task/book_store/tasks/views.py
+++ django_task/book_store/tasks/views.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.db.models import Q
 from django.utils import timezone
 
 from tasks.models import Task
@@ -7,23 +8,17 @@ from tasks.models import Task
 def fetch_task(worker_id):
     with transaction.atomic():
         stale_time = timezone.now() - timezone.timedelta(minutes=5)
-        stale_task = Task.objects.select_for_update(skip_locked=True).filter(
-            status='processing',
-            updated_at__lt=stale_time
-        ).order_by('created_at').first()
-
-        if stale_task:
-            stale_task.status = 'pending'
-            stale_task.worker_id = None
-            stale_task.save()
-
         task = Task.objects.select_for_update(skip_locked=True).filter(
-            status='pending'
+            Q(status='processing', updated_at__lt=stale_time) | Q(status='pending')
         ).order_by('created_at').first()
 
         if task:
-            task.status = 'processing'
-            task.worker_id = worker_id
+            if task.status == 'processing':
+                task.status = 'pending'
+                task.worker_id = None
+            else:
+                task.worker_id = 'processing'
+                task.worker_id = worker_id
             task.save()
 
     return task
